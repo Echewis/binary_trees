d8ba285131a59eba72f39d8aad3018080ce698bb
WisDomz <houseofwem1@gmail.com>
2024-01-03 23:38:29 +0100

    node siblings

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - Will find the node siblings
+ * @node: pointer to the node siblings
+ * Return: Null if node is Null
+ **/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
