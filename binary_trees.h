ac3ae6708bd74504c741bde67968e22bd5774e23
WisDomz <houseofwem1@gmail.com>
2024-01-03 11:07:16 +0100

    read me file

--- binary_trees.h
+++ binary_trees.h
@@ -10,6 +10,14 @@
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
 struct binary_tree_s
 {
 	int n;
@@ -27,8 +35,13 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 
 /* maximum binary heap */
-typedef struct binary_tree_s_heep_t;
+typedef struct binary_tree_s heep_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
 
 #endif
